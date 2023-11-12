2bc4a92b8e17c83c869ed6368e3b0054edf4d686
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2023-11-12 20:26:27 +0900

    required 반드시 추가할 번호 추가

--- random_2회_2중 랜덤.py
+++ random_2회_2중 랜덤.py
@@ -1,14 +1,19 @@
 import random
 
-def generate_numbers():
-    return random.sample(range(1, 9), 6)
+def generate_numbers(required_numbers):
+    all_numbers = list(set(list(range(1, 9)) + required_numbers))
+    return random.sample(all_numbers, 6)
 
 # 사용자로부터 items 값을 입력받아 리스트로 변환
-items_input = input("시도할 조 이름은?(다중선택 가능, 띄어쓰기로 구분) : ")
+items_input = input("시도할 조 이름은?(다중선택 가능, 띄어쓰기로 구분): ")
 items = items_input.split() if items_input else ['1조', '2조', '3조', '4조', '5조']
+
+# 사용자로부터 반드시 포함해야 하는 번호를 입력받아 리스트로 변환
+required_numbers_input = input("반드시 포함해야 하는 번호는?(띄어쓰기로 구분): ")
+required_numbers = [int(num) for num in required_numbers_input.split()]
 
 for _ in range(5):
     random_item = random.choice(items)
-    lotto_numbers = generate_numbers()
+    lotto_numbers = generate_numbers(required_numbers)
 
-    print("출력된 번호 :", random_item, lotto_numbers)
+    print("출력된 번호:", random_item, lotto_numbers)
