3b8f35ecd931e130ccf36e42d5c2ca09f1508812
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2023-11-12 20:33:20 +0900

    required_numbers_input값 미 입력시 [else]-랜덤으로

--- random_2회_2중 랜덤.py
+++ random_2회_2중 랜덤.py
@@ -6,15 +6,15 @@ def generate_numbers(required_numbers, exclude_numbers):
 
 # 사용자로부터 items 값을 입력받아 리스트로 변환
 items_input = input("시도할 조 이름은?(다중선택 가능, 띄어쓰기로 구분): ")
-items = items_input.split() if items_input else ['1조', '2조', '3조', '4조', '5조']
+items = items_input.split() if items_input else [f'{i}조' for i in range(1, 6)]
 
 # 사용자로부터 반드시 포함해야 하는 번호를 입력받아 리스트로 변환
-required_numbers_input = input("반드시 포함해야 하는 번호를 입력하세요(공백으로 구분): ")
-required_numbers = [int(num) for num in required_numbers_input.split()]
+required_numbers_input = input("반드시 포함해야 하는 번호를 입력하세요(띄어쓰기로 구분) : ")
+required_numbers = [int(num) for num in required_numbers_input.split()] if required_numbers_input else []
 
 # 사용자로부터 반드시 제외해야 하는 번호를 입력받아 리스트로 변환
-exclude_numbers_input = input("반드시 제외해야 하는 번호를 입력하세요(공백으로 구분): ")
-exclude_numbers = [int(num) for num in exclude_numbers_input.split()]
+exclude_numbers_input = input("반드시 제외해야 하는 번호를 입력하세요(띄어쓰기로 구분) : ")
+exclude_numbers = [int(num) for num in exclude_numbers_input.split()] if exclude_numbers_input else []
 
 for _ in range(5):
     random_item = random.choice(items)
