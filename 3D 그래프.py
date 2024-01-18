3ace288edd47b9363724f888d864859efc590128
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2024-01-18 22:40:46 +0900

    방향수정1

--- 3D 그래프.py
+++ 3D 그래프.py
@@ -1,13 +1,11 @@
-# pip install matplotlib 설치
-
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 # 데이터 생성
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
-z = np.linspace(-5, 5, 100)
+x = np.linspace(0, 3, 100)
+y = np.linspace(0, 4, 100)
+z = np.linspace(0, 5, 100)
 
 # 3D 그래프 생성
 fig = plt.figure()
@@ -21,6 +19,10 @@ ax.set_xlabel('X Axis')
 ax.set_ylabel('Y Axis')
 ax.set_zlabel('Z Axis')
 
+# 축의 한계를 설정하여 모든 축이 동일한 시작점에서 시작하도록 함
+ax.set_xlim([0, 5])
+ax.set_ylim([0, 5])
+ax.set_zlim([0, 5])
+
 # 그래프 표시
 plt.show()
-
