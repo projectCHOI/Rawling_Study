ff44d47533e0ea745a901a1c8fe05a5e647ddacd
최윤석 <144885127+projectCHOI@users.noreply.github.com>
2024-01-18 22:53:48 +0900

    하나의 점으로 표기

--- 3D 그래프.py
+++ 3D 그래프.py
@@ -2,16 +2,16 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
-# 데이터 생성
-x = np.linspace(0, 3, 100)
-y = np.linspace(0, 4, 100)
-z = np.linspace(0, 5, 100)
+# 데이터 생성: 하나의 점 (예: (1, 2, 3))
+x = [1]
+y = [2]
+z = [0]
 
 # 3D 그래프 생성
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# X, Y, Z 축에 데이터 플로팅
+# X, Y, Z 축에 데이터 플로팅 (하나의 점)
 ax.scatter(x, y, z)
 
 # 라벨 설정
@@ -19,7 +19,7 @@ ax.set_xlabel('X Axis')
 ax.set_ylabel('Y Axis')
 ax.set_zlabel('Z Axis')
 
-# 축의 한계를 설정하여 모든 축이 동일한 시작점에서 시작하도록 함
+# 축의 한계를 설정
 ax.set_xlim([0, 5])
 ax.set_ylim([0, 5])
 ax.set_zlim([0, 5])
