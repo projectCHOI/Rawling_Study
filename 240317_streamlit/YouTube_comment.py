0af52891115cb284232591bc22d23394d8d3e42a
yoonsuk <yoonsuk.choi93@gmail.com>
2024-03-17 20:54:46 +0900

    명령어 주석

--- 240317_streamlit/YouTube_comment.py
+++ 240317_streamlit/YouTube_comment.py
@@ -15,4 +15,6 @@ data = pd.read_csv(file_path)
 # 데이터를 화면에 표시
 st.write('YouTube Comments:', data)
 
+# cd C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\Rawling_Study\Rawling_Study\240317_streamlit
+
 # streamlit run YouTube_comment.py
