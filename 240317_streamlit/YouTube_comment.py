6ab683f9462f55310c1d71f2157b3276d419da1f
yoonsuk <yoonsuk.choi93@gmail.com>
2024-03-18 22:57:02 +0900

    labels_필터링

--- 240317_streamlit/YouTube_comment.py
+++ 240317_streamlit/YouTube_comment.py
@@ -12,8 +12,16 @@ file_path = r'C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\Rawling_Study\Rawl
 # CSV 파일 불러오기
 data = pd.read_csv(file_path)
 
-# 데이터를 화면에 표시
-st.write('YouTube Comments:', data)
+# 필터링할 라벨 선택
+label_to_filter = st.sidebar.selectbox(
+    'Select a label to filter:',
+    pd.unique(data['labels'])
+)
+
+# 필터링된 데이터 표시
+filtered_data = data[data['labels'] == label_to_filter]
+st.write('YouTube Comments filtered by label:', filtered_data)
+
 
 # cd C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\Rawling_Study\Rawling_Study\240317_streamlit
 
