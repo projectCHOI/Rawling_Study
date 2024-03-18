7229a74dd69efd3f0a7921ff1dde56b860342a58
yoonsuk <yoonsuk.choi93@gmail.com>
2024-03-18 23:08:42 +0900

    name, 댓글 필터링

--- 240317_streamlit/YouTube_comment.py
+++ 240317_streamlit/YouTube_comment.py
@@ -12,16 +12,27 @@ file_path = r'C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\Rawling_Study\Rawl
 # CSV 파일 불러오기
 data = pd.read_csv(file_path)
 
-# 필터링할 라벨 선택
-label_to_filter = st.sidebar.selectbox(
-    'Select a label to filter:',
-    pd.unique(data['labels'])
+# 사이드바 옵션
+# 'labels' 필터링 선택
+name_to_filter = st.sidebar.selectbox(
+    'Select a name to filter:',
+    ['All'] + list(pd.unique(data['name']))
 )
 
-# 필터링된 데이터 표시
-filtered_data = data[data['labels'] == label_to_filter]
-st.write('YouTube Comments filtered by label:', filtered_data)
+# '댓글'에 대한 키워드 검색
+keyword = st.sidebar.text_input('Enter a keyword to filter comments:', '')
 
+# 필터링 로직
+# 'name' 필터링
+if name_to_filter != 'All':
+    data = data[data['name'] == name_to_filter]
+
+# '댓글' 필터링
+if keyword:
+    data = data[data['댓글'].str.contains(keyword, case=False, na=False)]
+
+# 필터링된 데이터 표시
+st.write('Filtered YouTube Comments:', data)
 
 # cd C:\Users\HOME\Desktop\새싹_교육\GitHub_CHOI\Rawling_Study\Rawling_Study\240317_streamlit
 
